492a86fbc77e56bff9d6a108f98f1259769ea6d9
fermartini1 <hola.soymartini1992@gmail.com>
2025-04-10 23:21:34 -0300

    particulas de fondo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,8 +7,22 @@ export default {
 				cinzel: ['"Cinzel Decorative"', 'cursive'],
 				cormorant: ['"Cormorant Garamond"', 'serif'],
 				spectral: ['Spectral', 'serif'],
-			  },
+			},
+			animation: {
+				flotar1: "flotar1 20s ease-in-out infinite",
+				flotar2: "flotar2 20s ease-in-out infinite",
+			},
+			keyframes: {
+				flotar1: {
+					'0%, 100%': { transform: 'translate(0, 0)' },
+					'50%': { transform: 'translate(50px,500px)' },
+				},
+				flotar2: {
+					'0%, 100%': { transform: 'translate(0, 0)' },
+					'50%': { transform: 'translate(-30px,-500px)' },
+				},
+			},
 		},
-	},
-	plugins: [],
+		plugins: [],
+	}
 }
