be7879f251430bb71f26afd0af374d2fa37104f2
fermartini1 <hola.soymartini1992@gmail.com>
2025-04-18 19:56:36 -0300

    sinanalitic.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,12 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig({  
   
   site: "https://elpensadero.vercel.app",
-  integrations: [react(), tailwind(),mdx(),sitemap() ],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true, 
-    },
-  }),
-  
+  integrations: [react(), tailwind(),mdx(),sitemap() ]  
 });
